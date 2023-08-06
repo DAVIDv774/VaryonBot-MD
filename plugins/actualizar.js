af08877f3e4093d235dfc9c2c71f083fdb3a09eb
DAVID-774 <110315448+DAVIDv774@users.noreply.github.com>
2023-08-06 03:04:12 -0500

    Delete actualizar.js

--- plugins/actualizar.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process'
-let handler = async (m) => { var update = execSync('git remote set-url origin https://github.com/DAVIDv774/VaryonBot-MD.git && git pull'); m.reply(update.toString())}
-
-handler.help = ['actualizarbot']
-handler.tags = ['owner']
-handler.command = /^(actualizarbot|actualizar|update)$/i
-handler.rowner = true
-
-export default handler
