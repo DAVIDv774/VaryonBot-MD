14ce4031622d669eae3cdd32c3329c7f94f7460b
DAVID-774 <110315448+DAVIDv774@users.noreply.github.com>
2023-09-16 17:06:23 -0500

    Update ChatGpt.js

--- plugins/ChatGpt.js
+++ plugins/ChatGpt.js
@@ -2,7 +2,7 @@ import { GradioChatBot } from 'gradio-chatbot'
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
   if (!text) return m.reply('Y el texto?')
-  const bot = new GradioChatBot()
+  const bot = new GradioChatBot('10')
   m.react('\uD83D\uDCAC')
   async function start() { const message = await bot.chat(text, { onMessage(partialMsg) {/*console.log('stream output:', partialMsg)*/ } }); if(message) { conn.sendMessage(m.chat, { text: message, mentions: [m.sender] }, { ephemeralExpiration: 24 * 3600, quoted: { key: { participant: '0@s.whatsapp.net' }, message: { documentMessage: { title: `[IA] Desarrollado por OpenAI`, jpegThumbnail: fs.readFileSync('./multimedia/imagenes/mythumb.jpg')}}}})} else { m.reply('No se pudo completar la acción solicitada. Por favor, revisa los datos ingresados e inténtalo nuevamente.')}} start()
 }
